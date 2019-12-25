efa1074dc6c92be54e802a69dae3d5695cc7931a
Mohamad Hussain <mohamad.hussain@live.de>
2019-12-25 19:50:52 +0100

    initial commit

---
+++ src/main/java/com/mhussain/studium/seminargrundlagen/SeminarGrundlagenApplication.java
@@ -0,0 +1,13 @@
+package com.mhussain.studium.seminargrundlagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarGrundlagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarGrundlagenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhussain/studium/seminargrundlagen/SeminarGrundlagenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhussain.studium.seminargrundlagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarGrundlagenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
