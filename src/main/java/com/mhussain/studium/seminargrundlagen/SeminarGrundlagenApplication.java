137f80c54f98251f7f5cd3e5e23ec0db3e980345
Mohamad Hussain <mohamad.hussain@live.de>
2019-12-27 11:55:57 +0100

    implemented rest controller

--- src/main/java/com/mhussain/studium/seminargrundlagen/SeminarGrundlagenApplication.java
+++ src/main/java/com/mhussain/studium/seminargrundlagen/SeminarGrundlagenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeminarGrundlagenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeminarGrundlagenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeminarGrundlagenApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/mhussain/studium/seminargrundlagen/SemnarteilnehmerController.java
@@ -0,0 +1,38 @@
+package com.mhussain.studium.seminargrundlagen;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RequiredArgsConstructor
+@RequestMapping(path = "serminateilnehmer")
+@RestController
+public class SemnarteilnehmerController {
+
+    private final SerminarteilnehmerService serminarteilnehmerService;
+
+    @RequestMapping(method = GET)
+    public Collection<Seminarteilmehmer> findAll() {
+        return serminarteilnehmerService.findAll();
+    }
+
+    @RequestMapping(method = GET, path = "findByMatrikelnummer/{matrikelNummer}")
+    public Seminarteilmehmer findByMatrikelnummer(@PathVariable("matrikelNummer") Long matrikelNummer) {
+        return serminarteilnehmerService.findByMatrikelNummer(matrikelNummer)
+                .orElseThrow(() -> new IllegalArgumentException("Es konnte kein Seminarteilmehmer mit der Matrikelnummer: " + matrikelNummer + " gefunden werden!"));
+    }
+
+    @RequestMapping(method = POST)
+    public void create(@RequestBody Seminarteilmehmer seminarteilmehmer) {
+        serminarteilnehmerService.save(Collections.singletonList(seminarteilmehmer));
+    }
+
+}
